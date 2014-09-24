8204b82cd7554f2d3a4ec71ed55b2b7bbf4966ef
baardl <bard.lind@gmail.com>
2014-09-24 18:34:14 +0200

    Providing sla observations via database integration.

--- src/main/java/org/valuereporter/sla/SlaDao.java
+++ src/main/java/org/valuereporter/sla/SlaDao.java
@@ -5,9 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -25,8 +27,32 @@ public class SlaDao {
     }
 
     public List<UsageStatistics> findUsage(String prefix, String methodFilter, DateTime toNow, DateTime oneWeekOld) {
-        List<UsageStatistics> usageStatisticses = new ArrayList<>();
-        usageStatisticses.add(buildStub());
+        String sql = "select ok.prefix, ok.methodName, oi.duration,oi.startTime, oi.count, oi.max, oi.min, oi.mean, oi.median, oi.stdDev," +
+                "oi.p95, oi.p98, oi.p99  from ObservedInterval oi, ObservedKeys ok where ok.prefix=? and ok.methodName=? and ok.id = oi.observedKeysId order by oi.count desc, oi.max desc";
+
+        Object[] parameters = new Object[] {prefix,methodFilter};
+        List<UsageStatistics> usageStatisticses = jdbcTemplate.query(sql, parameters, new RowMapper<UsageStatistics>() {
+            @Override
+            public UsageStatistics mapRow(ResultSet resultSet, int i) throws SQLException {
+                //log.debug("Returned values: {},{},{},{},{}", resultSet.getObject(1),resultSet.getObject(2),resultSet.getObject(3),resultSet.getObject(4),resultSet.getObject(5));
+
+                UsageStatistics observedMethod = new UsageStatistics(
+                        resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getLong(3),
+                        resultSet.getTimestamp(4).getTime(),
+                        resultSet.getLong(5),
+                        resultSet.getLong(6),
+                        resultSet.getLong(7),
+                        resultSet.getDouble(8),
+                        resultSet.getDouble(9),
+                        resultSet.getDouble(10),
+                        resultSet.getDouble(11),
+                        resultSet.getDouble(12),
+                        resultSet.getDouble(13));
+                return observedMethod;
+            }
+        });
         return usageStatisticses;
 
     }
