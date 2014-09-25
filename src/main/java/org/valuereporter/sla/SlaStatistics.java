bc145e2a07648cc8aa4dafa32d3924c4c88a30e0
Bard Lind <bard.lind@tomra.com>
2014-09-25 15:19:21 +0200

    Renaming for better understanding.

--- src/main/java/org/valuereporter/sla/SlaStatistics.java
+++ src/main/java/org/valuereporter/sla/SlaStatistics.java
@@ -7,29 +7,29 @@ public class SlaStatistics {
 
     //Prefix and methoName is ommitted. This is to reduce MemoryFootprint from the selection.
     private long startTime;
-    private long duration;
+    private long interval;
     private long count;
     private double max;
     private double min;
     private double mean;
-    private double median;
+    private double p95;
 
-    public SlaStatistics(long startTime, long duration, long count, double max, double min, double mean, double median) {
+    public SlaStatistics(long startTime, long interval, long count, double max, double min, double mean, double p95) {
         this.startTime = startTime;
-        this.duration = duration;
+        this.interval = interval;
         this.count = count;
         this.max = max;
         this.min = min;
         this.mean = mean;
-        this.median = median;
+        this.p95 = p95;
     }
 
     public long getStartTime() {
         return startTime;
     }
 
-    public long getDuration() {
-        return duration;
+    public long getInterval() {
+        return interval;
     }
 
     public long getCount() {
@@ -48,7 +48,7 @@ public class SlaStatistics {
         return mean;
     }
 
-    public double getMedian() {
-        return median;
+    public double getP95() {
+        return p95;
     }
 }
