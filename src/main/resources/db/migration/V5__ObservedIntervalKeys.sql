1b4e8ff16c54a0acda634e626bae097e636cdd3b
Bard Lind <bard.lind@tomra.com>
2014-09-29 09:03:07 +0200

    Improved table creation.

--- src/main/resources/db/migration/V5__ObservedIntervalKeys.sql
+++ src/main/resources/db/migration/V5__ObservedIntervalKeys.sql
@@ -1,35 +1,37 @@
 USE ValueReporter
 GO
 
-CREATE TABLE [dbo].[ObservedKeys](
-  [id] [bigint] IDENTITY(1,1) PRIMARY KEY,
-  [prefix] [varchar](255) NOT NULL,
-  [methodName] [varchar](255) NOT NULL,
-  CONSTRAINT AK_Prefix_MethodName UNIQUE (prefix, methodName)
-);
+if not exists (select * from sysobjects where name='ObservedKeys' and xtype='U')
+  BEGIN
+    CREATE TABLE [dbo].[ObservedKeys](
+      [id] [bigint] IDENTITY(1,1) PRIMARY KEY,
+      [prefix] [varchar](255) NOT NULL,
+      [methodName] [varchar](255) NOT NULL,
+      CONSTRAINT AK_Prefix_MethodName UNIQUE (prefix, methodName)
+    );
+    insert into ObservedKeys(prefix, methodName) values ('initial', 'com.valuereporter.test');
+  End
 
-insert into ObservedKeys(prefix, methodName) values ('initial', 'com.valuereporter.test');
-
-
-create table [dbo].[ObservedInterval] (
-  [id] [bigint] IDENTITY(1,1) NOT NULL,
-  observedKeysId bigint NOT NULL ,
-  startTime datetime NOT NULL,
-  duration bigint not null,
-  count bigint NOT NULL,
-  max bigint not null,
-  min bigint not null,
-  mean decimal not null,
-  median decimal not null,
-  stdDev decimal not null,
-  p95 decimal,
-  p98 decimal,
-  p99 decimal,
-  FOREIGN KEY (observedKeysId) REFERENCES ObservedKeys(id)
-);
-
-insert into ObservedInterval (observedKeysId, startTime, duration, count, max, mean,min,median, stdDev)
-  select o.id, '2014-05-13T12:02:43.296', 15*60*1000, 4, 50, 5.0, 2,0,0
-  from ObservedKeys o
-  where prefix='initial' and methodName = 'com.valuereporter.test';
-
+if not exists (select * from sysobjects where name='ObservedInterval' and xtype='U')
+  BEGIN
+    create table [dbo].[ObservedInterval] (
+      [id] [bigint] IDENTITY(1,1) NOT NULL,
+      observedKeysId bigint NOT NULL ,
+      startTime datetime NOT NULL,
+      duration bigint not null,
+      count bigint NOT NULL,
+      max bigint not null,
+      min bigint not null,
+      mean decimal not null,
+      median decimal not null,
+      stdDev decimal not null,
+      p95 decimal,
+      p98 decimal,
+      p99 decimal,
+      FOREIGN KEY (observedKeysId) REFERENCES ObservedKeys(id)
+    );
+    insert into ObservedInterval (observedKeysId, startTime, duration, count, max, mean,min,median, stdDev)
+      select o.id, '2014-05-13T12:02:43.296', 15*60*1000, 4, 50, 5.0, 2,0,0
+      from ObservedKeys o
+      where prefix='initial' and methodName = 'com.valuereporter.test';
+  END
