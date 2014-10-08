10318712d19363ae1c7b243ed615513a87ce2481
Bard Lind <bard.lind@tomra.com>
2014-10-08 07:28:58 +0200

    #1 Fixing the bug.

--- src/test/java/org/valuereporter/observation/ObservationsServiceTest.java
+++ src/test/java/org/valuereporter/observation/ObservationsServiceTest.java
@@ -3,12 +3,24 @@ package org.valuereporter.observation;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static org.mockito.Mockito.mock;
+import static org.testng.Assert.assertTrue;
+
 public class ObservationsServiceTest {
+
+    private ObservationDao observationDao;
+    private ObservationsRepository observationsRepository;
+    private boolean persistAllMethodCalls = false;
+    private static final String PREFiX = "ObservationsServiceTest";
 
     @BeforeMethod
     public void setUp() throws Exception {
+        observationDao = mock(ObservationDao.class);
+        observationsRepository = mock(ObservationsRepository.class);
 
     }
+
+
 
     @Test
     public void testFindObservationsByName() throws Exception {
@@ -27,8 +39,14 @@ public class ObservationsServiceTest {
 
     @Test
     public void testIsScheduled() throws Exception {
+        int inteval = 10;
+        ObservationsService observationService = new ObservationsService(observationDao, observationsRepository, persistAllMethodCalls, inteval);
+        observationService.createScheduler(PREFiX);
+        assertTrue(observationService.isScheduled(PREFiX));
 
     }
+
+
 
     @Test
     public void testIsPersistMethodDetails() throws Exception {
