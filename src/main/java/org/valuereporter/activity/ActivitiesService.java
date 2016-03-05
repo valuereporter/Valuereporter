48631f6ccbfc057eb515f5e4a580519fac2e27c7
baardl <bard.lind@gmail.com>
2016-03-05 21:35:01 +0100

    #4 Support insert of multiple activity types.

--- src/main/java/org/valuereporter/activity/ActivitiesService.java
+++ src/main/java/org/valuereporter/activity/ActivitiesService.java
@@ -6,10 +6,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.valuereporter.whydah.LogonDao;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -31,25 +28,49 @@ public class ActivitiesService {
 
     public long updateActivities(String prefix, List<ObservedActivity> observedActivities) {
         long updatedActivities = 0;
+
         if (observedActivities != null && observedActivities.size() > 0) {
             log.trace("Try to update {} activities for {}", observedActivities.size(), prefix);
-            //TODO split into separate lambda expressions depending on activity.name
-            ObservedActivity activity = observedActivities.get(0);
-            String tableName = activity.getName();
-            Map<String, Object> data = activity.getData();
-            if (data != null && data.keySet() != null) {
-                Set<String> keys = data.keySet();
-                ArrayList<String> columnNames = new ArrayList<>(keys.size());
-                for (String key : keys) {
-                    columnNames.add(key);
+            //TODO split into separate lambda/clojure expressions depending on activity.name
+            Map<String,List<ObservedActivity>> activitiesByName = new HashMap<>();
+            String name = null;
+            for (ObservedActivity observedActivity : observedActivities) {
+                name = observedActivity.getName();
+                if (!activitiesByName.containsKey(name)){
+                    ArrayList<ObservedActivity> namedActivites = new ArrayList<>();
+                    namedActivites.add(observedActivity);
+                    activitiesByName.put(name, namedActivites);
+                } else {
+                    activitiesByName.get(name).add(observedActivity);
                 }
-                try {
+            }
+            Set<String> names = activitiesByName.keySet();
+            for (String activityName : names) {
+                List<ObservedActivity> activities = activitiesByName.get(activityName);
+                long updateCount = updateActivitiesByName(activities);
+                updatedActivities += updateCount;
+            }
+        }
+        return updatedActivities;
+    }
+
+    private long updateActivitiesByName(List<ObservedActivity> observedActivities) {
+        long updatedActivities = 0;
+        ObservedActivity activity = observedActivities.get(0);
+        String tableName = activity.getName();
+        Map<String, Object> data = activity.getData();
+        if (data != null && data.keySet() != null) {
+            Set<String> keys = data.keySet();
+            ArrayList<String> columnNames = new ArrayList<>(keys.size());
+            for (String key : keys) {
+                columnNames.add(key);
+            }
+            try {
+                updatedActivities = activitiesDao.insertActivities(tableName, columnNames, observedActivities);
+            } catch (DataAccessException de) {
+                if (isMissingTablexeption(de)) {
+                    createTable(tableName, columnNames, observedActivities);
                     updatedActivities = activitiesDao.insertActivities(tableName, columnNames, observedActivities);
-                } catch (DataAccessException de) {
-                    if (isMissingTablexeption(de)) {
-                        createTable(tableName, columnNames, observedActivities);
-                        updatedActivities = activitiesDao.insertActivities(tableName, columnNames, observedActivities);
-                    }
                 }
             }
         }
