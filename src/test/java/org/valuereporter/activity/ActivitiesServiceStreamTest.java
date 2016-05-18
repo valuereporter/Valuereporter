48bb813d713e701d66d428019a14b7317301361f
baardl <bard.lind@gmail.com>
2016-05-18 21:31:37 +0200

    Group count test.

--- src/test/java/org/valuereporter/activity/ActivitiesServiceStreamTest.java
+++ src/test/java/org/valuereporter/activity/ActivitiesServiceStreamTest.java
@@ -6,9 +6,9 @@ import org.testng.annotations.Test;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 import static org.testng.Assert.assertEquals;
 
 /**
@@ -28,20 +28,26 @@ public class ActivitiesServiceStreamTest {
 
     @Test
     public void testUpdateActivitiesByName() throws Exception {
-        List<String> list = new ArrayList<>();
+        List<ObservedActivity> list = new ArrayList<>();
 
-        list.add("Hello");
-        list.add("Hello");
-        list.add("World");
+        list.add(buildStubedActivity("logon"));
+        list.add(buildStubedActivity("Hello"));
+        list.add(buildStubedActivity("World"));
+        list.add(buildStubedActivity("logon"));
 
-        Map<String, Long> counted = list.stream()
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        Map<String, Long> counted = list.stream().collect(groupingBy(ObservedActivity::getName, counting()));
 
-        assertEquals(2, counted.get("Hello").longValue());
+        assertEquals(4, list.size());
+        assertEquals(2, counted.get("logon").longValue());
+        assertEquals(3, counted.size());
 
 
 
 
+    }
+
+    private ObservedActivity buildStubedActivity(String activity) {
+        return new ObservedActivity(activity, System.currentTimeMillis(),null);
     }
 
     @Test
